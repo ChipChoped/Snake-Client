114b85e44728ae8453f8dda12aebb38d3585bbed
Chaïmaâ Touhami <chaimaa-touhami@outlook.fr>
2023-02-24 19:38:28 +0100

    View asking the user to log in

---
+++ src/view/ViewLogIn.java
@@ -0,0 +1,83 @@
+package view;
+
+import org.json.JSONObject;
+import utils.User;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.Socket;
+import java.security.NoSuchAlgorithmException;
+
+import static functions.LogIn.logIn;
+
+public class ViewLogIn {
+    public ViewLogIn(Socket socket) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Snake Game Menu");
+        frame.setSize(new Dimension(500, 300));
+
+        Dimension windowSize = frame.getSize();
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        Point centerPoint = ge.getCenterPoint();
+        int dx = centerPoint.x - windowSize.width / 2;
+        int dy = centerPoint.y - windowSize.height / 2;
+        frame.setLocation(dx,dy);
+
+        JPanel mainPanel = new JPanel();
+        JPanel a = new JPanel();
+        JPanel b = new JPanel();
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(7, 1));
+
+        JLabel logInLabel = new JLabel("Log in to Snake!");
+        JLabel errorLabel = new JLabel();
+        JLabel usernameLabel = new JLabel("Username");
+        JLabel passwordLabel = new JLabel("Password");
+
+        JTextField usernameTextField = new JTextField(null, 15);
+        JPasswordField passwordField = new JPasswordField(null, 15);
+
+        JButton logInButton = new JButton("Log in");
+
+        logInButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                try {
+                    JSONObject jsonResponse = logIn(socket, usernameTextField.getText(), String.valueOf(passwordField.getPassword()));
+
+                    if (jsonResponse.get("type").equals("id")) {
+                        User user = new User(jsonResponse.getInt("id"), usernameTextField.getText());
+                        frame.dispose();
+                        ViewGameMenu viewGameMenu = new ViewGameMenu(user);
+                    }
+                    else if (jsonResponse.get("type").equals("error")) {
+                        errorLabel.setText((String) jsonResponse.get("message"));
+                        errorLabel.setForeground(Color.red);
+                    }
+                } catch (IOException | NoSuchAlgorithmException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        panel.add(logInLabel);
+        panel.add(errorLabel);
+        panel.add(usernameLabel);
+        panel.add(usernameTextField);
+        panel.add(passwordLabel);
+        panel.add(passwordField);
+        panel.add(logInButton);
+
+        mainPanel.add(a);
+        mainPanel.add(panel);
+        mainPanel.add(b);
+
+        frame.add(mainPanel);
+
+        frame.setVisible(true);
+    }
+}
